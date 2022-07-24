2c0e8167e56023ca5cd038edb55ad77e7d2b00c6
yutabee <bsk87m@gmail.com>
2022-07-25 00:18:20 +0900

    add delete hooks

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+import './App.css';
+import { Main } from './components/Main';
+import { Sidebar } from './components/Sidebar';
+import uuid from 'react-uuid'
+
+function App() {
+  const [notes, setNotes] = useState([]);
+
+  const onAddNote = () => {
+    console.log('新しくノートが追加されました');
+    const newNote = {
+      id: uuid(),
+      title: '新しいノート',
+      content: '新しいノートの内容',
+      modDate: Date.now(),
+    };
+    setNotes([...notes, newNote]);  
+    console.log(notes);
+  };
+
+  const onDeleteNote = (id) => {
+    const filterNotes = notes.filter((note) => note.id !== id);
+    setNotes(filterNotes);
+  }
+
+  return (
+    <div className="App">
+      <Sidebar onAddNote={onAddNote} onDeleteNote={onDeleteNote} notes={notes} />
+      <Main />
+    </div>
+  );
+}
+
+export default App;
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,23 +1,28 @@
 import React from 'react'
 import './Sidebar.css'
 
-export const Sidebar = () => {
+export const Sidebar = ({onAddNote,onDeleteNote, notes}) => {
   return (
     <>
         <div className="app-sidebar">
             <div className="app-sidebar-header">
                 <h1>ノート</h1>
-                <button>追加</button>
+                <button onClick={onAddNote}>追加</button>
             </div>
-            <div className="app-sidebar-notes">
-                <div className="app-sidebar-note">
-                    <div className="sidebar-note-title">
-                        <strong>タイトル</strong>
-                        <button>削除</button>
+              <div className="app-sidebar-notes">
+                  {notes.map((note) => (
+                    <div className="app-sidebar-note" key={note.id}>
+                        <div className="sidebar-note-title">
+                            <strong>{note.title}</strong>
+                            <button onClick={()=> onDeleteNote(note.id)}>削除</button>
+                        </div>
+                        <p>{note.content}</p>  
+                          <small>{new Date(note.modDate).toLocaleDateString('ja-JP', {
+                              hour: '2-digit',
+                              minute:'2-digit'
+                          })}</small>  
                     </div>
-                    <p>ノートの内容です</p>  
-                    <small>最後の修正日:xxx</small>  
-                </div>
+                  ))}    
             </div>
         </div>
     </>
